34e260face4533c3b49da460bb41149ca4506bfd
Bruno Righi <brunorighi267@gmail.com>
2025-02-27 12:40:39 -0300

    realizando os 2 exercicios solicitados na aula

--- aula_js_27-02/arrays.js
+++ aula_js_27-02/arrays.js
@@ -1,10 +1,11 @@
+// Uma classe possui métodos
 class MeuArray {
 
+    // Chamado quando damos NEW na classe
     constructor()
     {
         this.items = {}; // Usamos um objeto para armazenar os itens do array
         this.tamanho = 0; // Mantemos o controle do tamanho do array
-
     }
 
     // Adiciona um elemento no final do array
@@ -67,3 +68,58 @@ console.log(minha_variavel.tamanhoArray()); // Saída: 3
 
 console.log(minha_variavel.remover()); // Saída: 30 - pois remove o último elemento
 console.log(minha_variavel.tamanhoArray()); // Saída: 2
+
+console.table(minha_variavel.items);
+
+// ------------------------------EXERCICIOS------------------------------
+
+// ----- Exercicio 1
+
+// Instância
+const exer1 = new MeuArray();
+
+// Adicionando 5 tarefas no array
+exer1.adicionar("Realizar teste de mesa");
+exer1.adicionar("Imprimir a papelada do laboratorio");
+exer1.adicionar("Calcular a quantidade de valvulas por veiculo");
+exer1.adicionar("Fazer o almoço");
+exer1.adicionar("Jogar na noitada");
+
+
+// Removendo a última tarefa
+console.log("Tarefa removida: " + exer1.remover());
+
+// Exibir todas as tarefas utilizando o FOR
+for(let x = 0; x < exer1.tamanho; x++)
+{
+    let y = x + 1;
+    console.log(y + "° TAREFA: " + exer1.obterElemento(x));
+    y++;
+}
+
+
+
+// ----- Exercicio 2
+
+// Instância
+const exer2 = new MeuArray();
+
+// Adicionando 4 "funcionários"
+exer2.adicionar("Bruno Righi");
+exer2.adicionar("Cainã Bertolini");
+exer2.adicionar("Weverton Zoio");
+exer2.adicionar("Gabriel Coutinho");
+
+// Mostrando todos que foram adicionados
+for(let z = 0; z < exer2.tamanho; z++)
+{
+    console.log("Foi adicionado o : " + exer2.obterElemento(z));
+}
+
+
+// Mostrando o 3° funcionário
+console.log("O terceiro funcionario é o : " + exer2.obterElemento(2));
+
+// Limpando todos os registros do array
+exer2.limpar();
+console.log("REGISTROS REMOVIDOS");
