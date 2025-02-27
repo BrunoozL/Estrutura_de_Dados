346470fd0b4796047e191e5e22a3a3d8d1ce0e18
Bruno Righi <brunorighi267@gmail.com>
2025-02-27 10:55:17 -0300

    resolução do exercicio e adicionando a matriz multidimensional

--- aula_js_20-02/exercicios.js
+++ aula_js_20-02/exercicios.js
@@ -1,5 +1,18 @@
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function somaDosPares(array)
+{
+    let soma = 0;
 
-numeros.forEach(num => {
-    console.log(num + num);
-})
+    // Percorre o array
+    array.forEach(num => {
+        // Verifica se o número é par
+        if (num % 2 === 0) {
+            soma += num;
+        }
+    });
+
+    return soma;
+}
+
+// Exemplo de uso
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(somaDosPares(numeros)); // Saída: 20 (2 + 4 + 6 + 8)
--- aula_js_20-02/matrizes.js
+++ aula_js_20-02/matrizes.js
@@ -11,3 +11,14 @@ for(let i = 0; i < 3; i++)
 {
     console.log(nomes[i]);
 }
+
+// Matriz multidimensional
+
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+// O primeiro é LINHA e o segundo é COLUNA
+console.log(matriz[0][1]); // Saída: 2
