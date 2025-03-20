d6112283a4c72149fb9b28a4c0179bc140a2d378
Bruno Righi <brunorighi267@gmail.com>
2025-03-20 12:44:55 -0300

    aprendendo sobre a LinkedList (lista encadeada) e Node (node, next node)

---
+++ aula_js_20-03/linked_list.js
@@ -0,0 +1,119 @@
+// Classe Node para estruturar a nossa LinkedList
+class Node {
+    
+    constructor(value)
+    {
+        this.value = value; // 
+        this.next = null; // Ponteiro para o próximo nó
+    }
+}
+
+class LinkedList {
+
+    constructor()
+    {
+        this.head = null; // A lista começa vazia
+    }
+
+    // Inserir no ínicio da fila
+    insertAtBeginning(value)
+    {
+        let newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    // Inserir no fim da fila
+    insertAtEnd(value)
+    {
+        let newNode = new Node(value);
+
+        if(this.head === null)
+        {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while(current.next !== null)
+        {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    // Remover um nó por valor
+    removeByValue(value)
+    {
+        if (this.head === null)
+        {
+            return;
+        }
+
+        // Se o nó a ser removido ser o primeiro
+        if(this.head.value === value)
+        {
+            this.head = this.head.next;
+            return;
+        }
+
+        let current = this.head;
+        while(current.next !== null && current.next.value !== value)
+        {
+            current = current.next;
+        }
+
+        if(current.next !== null)
+        {
+            current.next = current.next.next;
+        }
+    }
+
+    // Buscar um nó por valor
+    find(value)
+    {
+        let current = this.head;
+
+        while(current !== null)
+        {
+            if(current.value === value)
+            {
+                return current;
+            }
+
+            current = current.next;
+        }
+
+        return null; // Retorna null se o valor não for encontrado
+    }
+
+    // Exibir a lista (opcional, para facilitar a vizualização)
+    printList()
+    {
+        let current = this.head;
+        let list = '';
+
+        while(current !== null)
+        {
+            list = list + current.value + ' -> ';
+            current = current.next;
+        }
+
+        console.log(list + 'null');
+    }
+}
+
+// Exemplos de uso:
+
+const  list = new LinkedList();
+
+// Inserindo no ínicio
+list.insertAtBeginning("2° Objeto");
+list.insertAtBeginning("1° Objeto");
+
+// Inserindo no fim
+list.insertAtEnd("3° Objeto");
+list.insertAtEnd("4° Objeto");
+
+console.log(list.printList());
